de765d63d390932f91f14c62cf86b0edb0d1edaa
mrunalinir <mrunalinir@users.noreply.github.com>
2026-01-30 00:10:58 +0000

    Deploying to gh-pages from @ mrunalinir/mrunalinir.github.io@7b317c7a767540ca57efee31c250e1598223b72b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Resume and CV. Download the PDF using the button above.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -511,7 +511,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Mrunalini_Ramnath_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
